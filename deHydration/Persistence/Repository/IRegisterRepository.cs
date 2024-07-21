b18f2fed89f352cef082cb0b2ed8046b876e8b3c
krkrys <krzys.krysiak@gmail.com>
2024-07-21 10:10:25 +0200

    technical commit and refactor

--- deHydration/Persistence/Repository/IRegisterRepository.cs
+++ deHydration/Persistence/Repository/IRegisterRepository.cs
@@ -5,11 +5,6 @@ namespace Persistence.Repository
 {
     public interface IRegisterRepository : IGenericRepository<User>
     {
-        Task<IEnumerable<User>> GetAllAsync();
-        Task<User?> GetByIdAsync(int id);
 
-        Task<int> AddAsync(User entity);
-        Task<bool> UpdateAsync(User entity);
-        Task<bool> DeleteAsync(int id);
     }
 }
