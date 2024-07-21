f18238cacb78bad7cc95084c77519c512402fbc4
krkrys <krzys.krysiak@gmail.com>
2024-07-21 09:52:27 +0200

    added Sypmtoms classesto be used in Examination

---
+++ deHydration/Domain/Models/Symptoms.cs
@@ -0,0 +1,30 @@
+﻿namespace Domain.Models
+{
+    public class Symptoms
+    {
+        public SymptomLevel GeneralAppearance { get; private set; }
+        public SymptomLevel RadialPulse { get; private set; }
+        public SymptomLevel Respirations { get; private set; }
+        public SymptomLevel AnteriorFontanelle { get; private set; }
+        public SymptomLevel SystolicBloodPressure { get; private set; }
+        public SymptomLevel SkinElasticity { get; private set; }
+        public SymptomLevel Eyes { get; private set; }
+        public SymptomLevel Tears { get; private set; }
+        public SymptomLevel MucousMembranes { get; private set; }
+
+        public Symptoms(SymptomLevel generalAppearance, SymptomLevel radialPulse,
+            SymptomLevel respirations, SymptomLevel anteriorFontanelle, SymptomLevel systolicBloodPressure,
+            SymptomLevel skinElasticity, SymptomLevel eyes, SymptomLevel tears, SymptomLevel mucousMembranes)
+        {
+            GeneralAppearance = generalAppearance;
+            RadialPulse = radialPulse;
+            Respirations = respirations;
+            AnteriorFontanelle = anteriorFontanelle;
+            SystolicBloodPressure = systolicBloodPressure;
+            SkinElasticity = skinElasticity;
+            Eyes = eyes;
+            Tears = tears;
+            MucousMembranes = mucousMembranes;
+        }
+    }
+}
